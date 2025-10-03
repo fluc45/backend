3c82d98754d8728327256114f4ed147c14685cf7
lvieira <vieira.luc@hotmail.com>
2025-10-02 21:51:51 -0300

    Aula 07

--- praticas/pratica05/routes/tarefaRouter.js
+++ praticas/pratica05/routes/tarefaRouter.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json([]);
+});
 
-router.get("/tarefas", []);
-
-
-
+router.get("/tarefas/:id", (req, res) => {
+    res.json([]);
+  });//???? 4.i)
 module.exports = router;
