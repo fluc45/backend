bcac5d165f42fad213c8c63dcba292f51e8a192c
lvieira <vieira.luc@hotmail.com>
2025-10-14 21:56:06 -0300

    Aula 09

---
+++ aulas/aula09/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const tarefaRouter = require("./routes/tarefaRouter");
+
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PSWD}@${process.env.MONGODB_HOST}/`;
+
+mongoose
+  .connect(url)
+  .then(() => console.log("Conectado no MongoDB"))
+  .catch((err) => {
+    console.log("Erro ao conectar no MongoDB", err.message);
+  });
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/tarefas", tarefaRouter);
+
+module.exports = app;
+
+//excluir 19 18 16 8 e 3 (depois de adicionar o requirie linha 1)
