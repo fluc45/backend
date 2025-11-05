b617034b6cbd4db56ed89fd2b8405e0b19cf6d8f
lvieira <vieira.luc@hotmail.com>
2025-11-04 21:42:24 -0300

    Aula 11

--- praticas/pratica08/middlewares/authMiddlewares.js
+++ praticas/pratica08/middlewares/authMiddlewares.js
@@ -1,4 +1,33 @@
 const jwt = require("jsonwebtoken");
 
 function verificarToken(req, res, next) {
+    const { authorization } = req.headers;
+    try {
+        const token = authorization.split(" ")[1];
+        const payload = jwt.verify(
+            token, 
+            process.env.JWT_SECRET
+        );
+        req.payload = {
+            usuario: payload.iss
+        };
+    } catch (err) {
+        res.status(401).json({msg: "Token invalido"})
+    }
 }
+
+function gerarToken(payload){
+    const expiresIn = 120; //2min
+    try {
+        const token = jwt.sign(
+            payload, 
+            process.env.JWT_SECRET,
+            { expiresIn }
+        );
+        return token;
+    } catch(err) {
+        throw Error("Erro ao gerar token");
+    }
+}
+
+module.exports = { verificarToken, gerarToken }
